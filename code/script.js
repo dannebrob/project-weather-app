c1c0c45a79d457336a135c6c0b4ff57f1e99d79c
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-02-25 21:59:15 +0100

    Clean up of script and css

--- code/script.js
+++ code/script.js
@@ -18,7 +18,6 @@ const bodyContainer = document.querySelector(".body-container");
 
 //Global variables
 let apiResponse, nightTemp, crd, latitude, longitude;
-
 let date = new Date();
 
 const stylingTimer = () => {
@@ -28,39 +27,18 @@ const stylingTimer = () => {
     bodyContainer.classList.add("night");
   }
 };
-stylingTimer();
 
 //API Fetch()
-
-// const weatherFetch = (city, countryCode) => {
-//   const apiKey = "191f229fc0a6e0f86812a75292074cb9";
-//   fetch(
-//     `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&units=metric&APPID=${apiKey}`
-//   )
-//     .then((response) => response.json())
-//     .then((data) => {
-//       apiResponse = data;
-//       console.log(apiResponse);
-//       const sunriseTime = new Date(apiResponse.sys.sunrise * 1000); // API date/time value needs to be multiplied by 1000 for .toLocaleTimeString() to return the correct value
-//       //console.log(sunriseTime.toLocaleTimeString())
-//       sunGoesUp = sunriseTime.toLocaleTimeString();
-//       const sunSetTime = new Date(apiResponse.sys.sunset * 1000);
-//       sunGoesDown = sunSetTime.toLocaleTimeString();
-//     });
-// };
-
 const weatherFetch = (lat, lon) => {
   const apiKey = "191f229fc0a6e0f86812a75292074cb9";
   latitude = lat;
   longitude = lon;
   fetch(
-    // `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&units=metric&APPID=${apiKey}`
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`
   )
     .then((response) => response.json())
     .then((data) => {
       apiResponse = data;
-      console.log(apiResponse);
       const sunriseTime = new Date(apiResponse.sys.sunrise * 1000);
       sunGoesUp = sunriseTime.toLocaleTimeString([], { timeStyle: "short" }); //why doesn't it work to just put this at the end of sunriseTime?
       const sunSetTime = new Date(apiResponse.sys.sunset * 1000);
@@ -69,27 +47,7 @@ const weatherFetch = (lat, lon) => {
     });
 };
 
-//Geolocation api
-const options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0,
-};
-
-function success(pos) {
-  crd = pos.coords;
-  console.log("Your current position is:");
-  console.log(`Latitude : ${crd.latitude}`);
-  console.log(`Longitude: ${crd.longitude}`);
-  console.log(`More or less ${crd.accuracy} meters.`);
-}
-
-function error(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-}
-
-//Async before it was cool...this is just a timer to wait for the response
-//Since the fetch() will be async we need to fix this :)
+//Updates the html after 1000ms
 const loadHtml = () => {
   setTimeout(() => {
     currentCity.innerHTML = `${apiResponse.name}`;
@@ -103,8 +61,6 @@ const loadHtml = () => {
   }, 1000);
 };
 
-//let date = new Date(time);
-
 let clearWeekList = () => {
   weekList.innerHTML = ``;
 };
@@ -116,7 +72,6 @@ const getForecastData = () => {
   )
     .then((response) => response.json())
     .then((data) => {
-      //forecastResponse = data;
       const filteredForecast = data.list.filter((item) =>
         item.dt_txt.includes("09:00")
       ); //Makes a 5 day array read at 12
@@ -125,7 +80,6 @@ const getForecastData = () => {
       const filteredForecastNight = data.list.filter((item) =>
         item.dt_txt.includes("21:00")
       );
-      console.log(filteredForecastNight);
 
       filteredForecast.forEach((day) => {
         const date = new Date(day.dt * 1000);
@@ -146,21 +100,11 @@ const getForecastData = () => {
     });
 };
 
-//event listeners
+//Event Listeners
 toggleNav.addEventListener("click", () => {
   cityList.classList.toggle("hidden");
 });
-/*
-myLocation.addEventListener("click", () => {
-  navigator.geolocation.getCurrentPosition(success, error, options);
-  setTimeout(() => {
-    weatherFetch(latitude, longitude);
-    latitude = crd.latitude;
-    longitude = crd.longitude;
-    cityList.classList.toggle("hidden");
-    loadHtml();
-  }, 3000); 
-})*/
+
 cityOne.addEventListener("click", () => {
   cityList.classList.toggle("hidden");
   weatherFetch("57.708870", "11.974560");
@@ -186,12 +130,6 @@ arrowButton.addEventListener("click", () => {
 });
 
 //Loads the weather
+stylingTimer();
 weatherFetch("59.6173", "16.5422");
 loadHtml();
-
-//TO DO
-//add a logo to the top right that changes depending on time and weather.
-//Put in another city and current location in the nav menu?
-//either make the arrow button show more info about weather, or remove it
-// styling and media queries
-//fix suntime to show hh:mm and not seconds + am/pm
